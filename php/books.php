ce74ecf7a072f3ceeacffe82620f74c5812dff11
AtharvaJoshi1302 <110755376+AtharvaJ1302@users.noreply.github.com>
2024-01-18 22:29:47 +0530

    commit

--- php/books.php
+++ php/books.php
@@ -4,21 +4,111 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/books.css">
+
+     <!-- font awesome cdn -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+      <!-- bootstrap 5 cdn -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- bootstrap 5 js bundle cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <div class="featured__container container">
-        <div class ="featured__swiper">
+
+
+<!-- Header section -->
+
+<header>
+        <div class="header1">
+            <a href="" class="logo"><img src="../images/logo.png" alt="" width="100px" height="70px"></a>
+            <nav class="navbar">
+                <a href="index.php">Home</a>
+                <a href="books.php">Books</a>
+                <a href="about.php">About</a>
+                <a href="suggestion.php">Suggestion</a>
+
+
+            </nav>
+
+            <div class="icons">
+                <!-- <div id="login-btn" class="fas fa-user"></div> -->
+                <!-- <a href="#login">Login</a> -->
+                <h3>Book Hunt</h3>
+            </div>
+        </div>
+    </header>
+
+    <!-- Header section -->
+
+
+    <div class="featured__container container m-1 ">  
             <div>
                 <article class="featured__card">
                     <img src="../uploads/cover/A Little Life.png" alt="" class="featured__img">
 <h2 class="featured__title">Featured Book</h2>
 <h4 class="featured__author">By:Demo author</h4>
-</div>
-<button class="button">View</button>
-<button class="button">Download</button>
+<button class="btn btn-success"><a href="../uploads/files/A Little Life.pdf">View</a></button>
+<button class="btn btn-success">Download</button>
 </article>
 </div>
+           
 </div>
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+<section class="footer">
+    <div class="box-container">
+    <div class="box">
+        <h3>Quick Links</h3>
+        <a href=""><i class="fas fa-arrow-right"></i>Home</a>
+        <a href=""><i class="fas fa-arrow-right"></i>Books</a>
+        <a href=""><i class="fas fa-arrow-right"></i>About</a>
+        <a href=""><i class="fas fa-arrow-right"></i>Support</a>
+    </div>
+
+    <div class="box">
+        <h3>Contact Info</h3>
+        <a href=""><i class="fas fa-phone"></i> +91 9864287530</a>
+        <a href=""><i class="fas fa-phone"></i> +91 9748561302</a>
+        <a href=""><i class="fas fa-envelope"></i> bookhunt2024@gmail.com</a>
+    </div>
+</div>
+
+    <div class="share">
+        <a href="" class="fab fa-facebook-f"></a>
+        <a href="" class="fab fa-twitter"></a>
+        <a href="" class="fab fa-instagram"></a>
+        <a href="" class="fab fa-linkedin"></a>
+        <a href="" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="credit">
+        Created by <span>Janhavi Dehadray</span> | All rights reserved!
+    </div>
+</section>
+
+<!-- footer section -->
+
+
+
+
+
+    <script src="../JS/index.js"></script>
+
     
 </body>
 </html>
