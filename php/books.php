a995b8a0bf88af9337008a39e31b8ca8e33d1997
AtharvaJoshi1302 <110755376+AtharvaJ1302@users.noreply.github.com>
2024-01-20 22:11:27 +0530

    commit

--- php/books.php
+++ php/books.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/books.css">
+    <link rel="stylesheet" href="../css/index.css">
+
+     <!-- bootstrap 5 cdn -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<!-- bootstrap 5 js bundle cdn -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
 
@@ -40,8 +50,7 @@
 <h2 class="featured__title">Featured Book</h2>
 <h4 class="featured__author">By:Demo author</h4>
 </div>
-<button class="button">View</button>
-<button class="button">Download</button>
+<button class="btn btn-primary">Download</button>
 </article>
 </div>
 </div>
