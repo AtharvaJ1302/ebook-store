3e2d06dd8a7e7eb22ad00d8b345c63ea7d80bfb2
JanhaviDehadray <151810382+JanhaviDehadray@users.noreply.github.com>
2024-01-20 21:54:33 +0530

    commit

--- php/books.php
+++ php/books.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/books.css">
+     <!-- font awesome cdn -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
 </head>
 <body>
     <div class="featured__container container">
@@ -12,13 +17,15 @@
                 <article class="featured__card">
                     <img src="../uploads/cover/A Little Life.png" alt="" class="featured__img">
 <h2 class="featured__title">Featured Book</h2>
-<h4 class="featured__author">By:Demo author</h4>
-</div>
+<h4 class="featured__author">By:Hanya Yanagihara</h4>
+
+
 <button class="button">View</button>
 <button class="button">Download</button>
+</div>
 </article>
 </div>
-</div>
+
     
 </body>
 </html>
